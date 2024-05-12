c28bd36c3f7d950ff99edbb173c31328ce895a8c
Shehraz Ali <Shehrazali365@gmail.com>
2024-05-12 16:57:42 +0500

    .

---
+++ client/src/app/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from './page.module.css'; // Import CSS module
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push('/login');
+  };
+
+  const handleLogout = () => {
+    signOut();
+  };
+
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.wrapper}>
+        {status !== "authenticated" ? (
+          <>
+            <h2 className={styles.heading}>Welcome to our website</h2>
+            <hr className={styles.hrLine} />
+            <p className={styles.paragraph}>Please sign in or create an account to continue.</p>
+            <button className={styles.btn} onClick={handleLogin}>Sign In / Sign Up</button>
+          </>
+        ) : (
+          <>
+            <h2 className={styles.heading}>Welcome, {session.user.name}</h2>
+            <hr className={styles.hrLine} />
+            <p className={styles.paragraph}>Email: {session.user.email}</p>
+            <button className={styles.btn} onClick={handleLogout}>Sign out</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
